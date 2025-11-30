c58789ba762518009b277fe96aea004c667d8fc8
Mohamed Akeel Khan <Mohamedakeelkhan@gmail.com>
2025-11-30 14:10:28 -0500

    basic server setup and connected to mongo

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import errorHandler from './middleware/errorHandler.js';
+
+// Load env variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Test Route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Error handler
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
